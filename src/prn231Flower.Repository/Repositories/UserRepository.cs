39cc9fe6875e73482953b979127ae6c5f8d848d2
Kobii24 <quochuyho220603@gmail.com>
2024-10-11 08:21:54 +0700

    initial entities repository

---
+++ src/prn231Flower.Repository/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using prn231Flower.Data.Base;
+using prn231Flower.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prn231Flower.Repository.Repositories;
+
+public class UserRepository : GenericRepository<User>
+{
+    public UserRepository(DatabaseContext context) => _context = context;
+}
